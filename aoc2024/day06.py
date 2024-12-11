b1ad2232518681fb926611c4fe624567d27a1b2b
JIE-CHI <chijie1015@gmail.com>
2024-12-11 01:15:11 +0100

    day06

--- aoc2024/day06.py
+++ aoc2024/day06.py
@@ -1,56 +1,59 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Fri Dec  6 22:05:29 2024
+Created on Tue Dec 10 23:39:20 2024
 
 @author: jiechi
 """
-from collections import defaultdict
-from functools import cmp_to_key
 
-f_in = open('inputs/day05.txt').read()
-# f_in = open('test.txt').read()
-rules, updates = f_in.split('\n\n')
-Rs = defaultdict(int)
+f_in = open('inputs/day06.txt').readlines()
+# f_in = open('test.txt').readlines()
+G = []
 
-for line in rules.split('\n'):
-    nums = list(map(int, line.split('|')))
-    Rs[nums[0], nums[1]] = 1
-    
-# for x in Rs.keys():
-#     for y, yl in Rs.items():
-#         if x in yl:
-#             Rs[y]= set(Rs[x]) | set(Rs[y])
-          
-
-def check(update):
-    for num in range(len(update)-1):
-        for prev in range(num+1, len(update)):
-            if Rs[update[prev] , update[num]] == 1:
-                return False
-    return True
+for r, line in enumerate(f_in):
+    if '^' in line:
+        S = (r, line.index('^'))
+    G.append(list(line.strip()))
 
 
-def cmp(x,y):
-    if Rs[x,y] == 1:
-        return -1
-    elif Rs[y,x] == 1:
-        return 1
-    return 0
-    
-    
-p1 = 0
-p2 = 0
-for line in updates.strip().split('\n'):
-    update = list(map(int, line.split(',')))
-    if check(update):
-        p1 += update[len(update)//2]
+D = [(-1, 0), (0,1), (1,0), (0,-1) ]
+i = 0
+
+def check(S,i):
+    r,c = S
+    p2 = set()
+    while (0 < r < len(G)-1 and  0 < c < len(G[0])-1):
+        d = D[i%4]
+        if G[r+d[0]][c+d[1]] in ['.', '^']:
+            r += d[0]
+            c += d[1]
+            if (r,c, d) in p2:
+                return True
+            else:
+                p2.add((r,c, d))
+        else:
+            i+= 1
+    return False
+
+
+r,c = S
+p2 = 0 
+p1 = set([S])
+
+
+while (0 < r < len(G)-1 and  0< c < len(G[0])-1):  
+    d = D[i%4]
+    if G[r+d[0]][c+d[1]] in ['.', '^']:
+        if not(r+d[0], c+d[1]) in p1:
+            G[r+d[0]][c+d[1]] = 'O'
+            if check((r,c),i):    
+                p2 += 1
+            G[r+d[0]][c+d[1]] = '.'
+        r += d[0]
+        c += d[1]
+        p1.add((r,c))
     else:
-        update = sorted(update, key=cmp_to_key(cmp))
-        p2 += update[len(update)//2]
-
-print(p1)
-print(p2)
-                
+        i+= 1
         
-    
+print(len(p1))
+print(p2)
